f7ef72ea90ab491702bfd8697b6d949fdc412d1c
熊小酒 <xiongjiuming38@foxmail.com>
2021-12-14 13:22:24 +0800

    完成首页的样式修改，路由增加返回的展示数据

--- app/main/views.py
+++ app/main/views.py
@@ -19,14 +19,30 @@ def index(): # 数据汇总
     def amount_(direction='PAY', is_total_direction=True, year=None, month=None, day=None):
         search = and_(Bill.direction==direction)
 
-        if year is not None:
-            search = and_(search, extract('year', Bill.accounting_time) == today.year)
-            if month is not None:
-                search = and_(search, extract('month', Bill.accounting_time) == month)
-                if day is not None:
+        if year == None: # 查询 月日
+            if month !=None:
+                if day == None: # 查询月日
+                    search = and_(search, extract('day', Bill.accounting_time) == day, extract('month', Bill.accounting_time) == month)
+                else: # 查询月:
+                    search = and_(search, extract('month', Bill.accounting_time) == month)
+            else:
+                if day != None: ## 查询日
                     search = and_(search, extract('day', Bill.accounting_time) == day)
 
+        else: # 查询年月日
+            if month == None: ## 查询年日
+                if day != None: ### 查询年日
+                    search = and_(search, extract('day', Bill.accounting_time) == day, extract('year', Bill.accounting_time) == year)
+                else: # 查询年
+                    search = and_(search, extract('year', Bill.accounting_time) == year)
+            else: # 查询年月
+                if day == None: # 查询年月
+                    search = and_(search, extract('month', Bill.accounting_time) == month, extract('year', Bill.accounting_time) == year)
+                else: ## 查询年月日
+                    search = and_(search, extract('day', Bill.accounting_time) == day, extract('month', Bill.accounting_time) == month, extract('year', Bill.accounting_time) == year)
+
         amount = db.session.query(func.sum(Bill.amount)).filter(search).scalar()
+        print(str(db.session.query(func.sum(Bill.amount)).filter(search)))
         amount = 0 if amount == None else '%.2f' % amount
 
         if is_total_direction == True:
@@ -39,27 +55,30 @@ def index(): # 数据汇总
         return amount
 
     # 金额
-    ## 总金额支出
-    total_amount = amount_()
+    ## 总金额
+    total_amount = amount_() # 所有发生额
+    total_today_amount = amount_(day=today.day) # 本日的所有发生额
+    total_month_amount = amount_(month=today.month) #本月的所有发生额
 
-    ## 今年金额支出和收入
-    this_year_amount = amount_(year=today.year)
+    this_year_amount = amount_(year=today.year) ## 本年金额支
+    this_month_amount = amount_(year=today.year, month=today.month) ## 本月金额
+    today_amount = amount_(year=today.year, month=today.month, day=today.day) ## 今天金额
     
-    ## 本月金额支出和收入
-    this_month_amount = amount_(year=today.year, month=today.month)
+    last_year_amount = amount_(year=today.year-1) ## 去年金额
+    last_month_amount = amount_(year=today.year-1, month=today.month-1) ## 上月金额
 
-    ## 今天金额
-    today_amount = amount_(year=today.year, month=today.month, day=today.day)
+    ## 同比
+    ony_month_amont = amount_(year=today.year-1, month=today.month) # 去年的本月
+    ony_today_amont = amount_(today.month-1, day=today.day)# 上月今天
 
-    ## 去年金额
-    last_year_amount = amount_(year=today.year-1)
 
-    ## 上月金额
-    last_month_amount = amount_(year=today.year-1, month=today.month-1)
+    
 
     ## 全部金额的字典
     amount = {
         "total_amount": total_amount,
+        "total_today_amount": total_today_amount,
+        "total_month_amount": total_month_amount,
         "this":{
             "year": this_year_amount,
             "month": this_month_amount,
@@ -67,7 +86,11 @@ def index(): # 数据汇总
         "last":{
             "year":last_year_amount,
             "month":last_month_amount
-        }
+        },
+        "ony":{
+            "month": ony_month_amont,
+            "day":ony_today_amont
+        },
             }
     return render_template('index.html', amount=amount)
 
