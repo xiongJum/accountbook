2e2a98f0143e206eef917f37d43e8ff238c7c78f
Xiong Jum <xiongjum96@outlook.com>
2022-01-08 21:42:01 +0800

    增加功能，修改明细和删除明细

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,16 +1,33 @@
 import datetime
 from flask_wtf import FlaskForm
 from wtforms import FloatField, StringField, TextAreaField, BooleanField, SubmitField, DateField
+from wtforms import SelectField
 from wtforms.validators import DataRequired
 
 class WriteForm(FlaskForm):
     # account_book = StringField("账本", validators=[DataRequired])
-    today = datetime.date.today()
     timestart = DateField("日期",default=None)
     amount = FloatField("金额", validators=[DataRequired()])
     # direction = BooleanField("方向", validators=[DataRequired()])
     tag = StringField("标签", validators=[DataRequired()])
     remark = TextAreaField("备注")
     submit = SubmitField("记账")
+
+class EditItemsForm(FlaskForm):
+    """修改账本"""
+
+    account_book = StringField("账本", validators=[DataRequired()])
+    timestart = DateField("日期",validators=[DataRequired()])
+    amount = FloatField("金额", validators=[DataRequired()])
+    direction = SelectField ("方向", coerce=bool)
+    tag = StringField("标签", validators=[DataRequired()])
+    remark = TextAreaField("备注")
+    submit = SubmitField("修改")
+
+    def __init__(self, *args, **kwargs):
+        super(EditItemsForm, self).__init__(*args, **kwargs)
+        self.direction.choices = [(False, "支出"), (True, "记账")]
+        
+
     
 
--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,7 @@
 import datetime
 from flask import render_template, session, redirect, url_for, flash, request, current_app
 from . import main
-from . forms import WriteForm
+from . forms import WriteForm, EditItemsForm
 from .. import db
 from ..models import Book
 
@@ -31,6 +31,7 @@ def write(directions):
 
 @main.route('/book', methods=["GET", "POST"])
 def book():
+
     books = Book.query
     page = request.args.get('page', 1, type=int)
     if page == -1: # 计算评论的总量和总也是，得出真正显示的页数
@@ -39,3 +40,36 @@ def book():
         page, per_page=current_app.config['FLASKY_COMMENTS_PER_PAGE'], error_out=False)
     books = pagination.items
     return render_template('book.html', books=books, pagination=pagination)
+
+@main.route('/edit-item<int:id>', methods=["GET", "POST"])
+def edit_item(id):
+    book = Book.query.get_or_404(id)
+    form = EditItemsForm()
+    if form.validate_on_submit():
+        book.timestart = form.timestart.data
+        book.amount = form.amount.data
+        book.direction = form.direction.data
+        book.tag = form.tag.data
+        book.remark = form.remark.data
+        book.account_book = form.account_book.data
+        db.session.add(book)
+        db.session.commit()
+        flash("账单已更新")
+        return redirect(url_for('.book'))
+    form.timestart.data = book.timestart
+    form.amount.data = book.amount
+    form.direction.data = book.direction
+    form.tag.data = book.tag
+    form.remark.data = book.remark
+    form.account_book.data = book.account_book
+    return render_template('edit_item.html', form=form)
+
+@main.route('/del-item/<int:id>', methods=["GET", "POST"])
+def del_item(id):
+    book = Book.query.get_or_404(id)
+    db.session.delete(book)
+    db.session.commit()
+    flash("账单已删除")
+    return redirect(url_for('.book'))
+
+
